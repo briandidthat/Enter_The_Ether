21ab08b98b136795107c7f5498cca0888c12363a
Blora616 <Blora616@gmail.com>
2019-04-19 00:53:16 -0400

    progress

--- ethernode/src/components/Layout/Header/Nav/index.js
+++ ethernode/src/components/Layout/Header/Nav/index.js
@@ -2,17 +2,21 @@ import React from "react";
 import "./Nav.css"
 
 //functional component for the Navbar
-const Nav = () => (
+const Nav = () => {
+    return (
     <div className="menu-header">
         <div className="nav">
             <nav>
                 <ul>
-                    <li><a href="index.html">Home</a>  </li>
+                    <li><a href="/">Home</a></li>
                     <li><a href="https://forum.ethernode.io">Forum</a></li>
+                    <li><a href="contact">Contact</a></li>
+                    <li><a href="product">Product</a></li>
                 </ul>
             </nav>
         </div>
     </div>
-);
+  )
+};
 
 export default Nav;
