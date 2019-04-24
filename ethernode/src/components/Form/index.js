9e61c9caf70ce4a95c68e50b097f5c7205d3e67d
Blora616 <Blora616@gmail.com>
2019-04-24 14:21:32 -0400

    contactForm to functional

--- ethernode/src/components/Form/index.js
+++ ethernode/src/components/Form/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useReducer } from 'react'
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardHeader from "@material-ui/core/CardHeader";
@@ -6,108 +6,120 @@ import TextField from "@material-ui/core/TextField";
 import Button from "../../components/UI/Button";
 import { Container, Row, Col } from "react-grid-system";
 
-export class ContactForm extends Component {
+//Functional 
+const ContactForm = () => {
+    //Set initial state using a reducer since we have multiple input fields
+    const [state, setState] = useReducer(
+    (state, newState) => ({...state, ...newState}),
+    {firstName: " ", lastName: " ", email: " ", message: " "}
+    );
+    //destructure values from the state to access easily
+    const { firstName, lastName, email, message } = state;
 
-    state = {
-        firstName: " ",
-        lastName: " ",
-        email: " ",
-        message: " "
-    }
-
-    handleChange = name => event => {
-        this.setState({
-            [name]: event.target.value,
-        });
-    };
-
-    handleFormSubmit = e => {
+    function handleFormSubmit(e) {
         e.preventDefault();
-        console.log(this.state);
-    };
-
-    render() {
-        return (
-            <React.Fragment>
-                <br />
-                <Container>
-                    <Row>
-                        <Col lg={6}>
-                            
-                            <Card style={styles.card}>
-                                <CardHeader
-                                    title="Contact Us"
-                                >
-                                </CardHeader>
-                                <CardContent>
-                                    <Row>
-                                        <Col md={6}>
-                                            <TextField
-                                                id="outlined-name"
-                                                label="First Name"
-                                                fullWidth
-                                                value={this.state.firstName}
-                                                onChange={this.handleChange('firstName')}
-                                                margin="normal"
-                                                variant="outlined"
-                                            />
-                                        </Col>
-                                        <Col md={6}>
-                                            <TextField
-                                                id="outlined-name"
-                                                label="Last Name"
-                                                value={this.state.lastName}
-                                                fullWidth
-                                                onChange={this.handleChange('lastName')}
-                                                margin="normal"
-                                                variant="outlined"
-                                            />
-                                        </Col>
-                                    </Row>
-
-                                    <Row>
-                                        <Col lg={12}>
-                                            <TextField
-                                                id="outlined-name"
-                                                label="Email"
-                                                type="email"
-                                                fullWidth
-                                                value={this.state.email}
-                                                onChange={this.handleChange('email')}
-                                                margin="normal"
-                                                variant="outlined"
-                                            />
-                                        </Col>
-                                    </Row>
-                                    <Row>
-                                        <Col lg={12}>
-                                            <TextField
-                                                id="outlined-name"
-                                                label="Message"
-                                                fullWidth
-                                                multiline
-                                                value={this.state.message}
-                                                onChange={this.handleChange('message')}
-                                                margin="normal"
-                                                variant="outlined"
-                                            />
-                                        </Col>
-                                    </Row>
-                                    <Button
-                                        className="button button-accent"
-                                        onClick={this.handleFormSubmit}
-                                    >   Submit
-                                    </Button>
-                                </CardContent>
-                            </Card>
-                        </Col>
-                    </Row>
-                </Container>
-                <br />
-            </React.Fragment>
-        )
+        console.log(state);
     }
+
+    return (
+        <React.Fragment>
+            <br />
+            <Container>
+                <Row>
+                    <Col lg={6}>
+
+                        <Card style={styles.card}>
+                            <CardHeader
+                                title="Contact Us"
+                            >
+                            </CardHeader>
+                            <CardContent>
+                                <Row>
+                                    <Col md={6}>
+                                        <TextField
+                                            id="outlined-name"
+                                            label="First Name"
+                                            fullWidth
+                                            value={firstName}
+                                            onChange={e => setState({ firstName: e.target.value})}
+                                            margin="normal"
+                                            variant="outlined"
+                                        />
+                                    </Col>
+                                    <Col md={6}>
+                                        <TextField
+                                            id="outlined-name"
+                                            label="Last Name"
+                                            value={lastName}
+                                            fullWidth
+                                            onChange={e => setState({ lastName: e.target.value})}
+                                            margin="normal"
+                                            variant="outlined"
+                                        />
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col lg={12}>
+                                        <TextField
+                                            id="outlined-name"
+                                            label="Email"
+                                            type="email"
+                                            fullWidth
+                                            value={email}
+                                            onChange={e => setState({ email: e.target.value})}
+                                            margin="normal"
+                                            variant="outlined"
+                                        />
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col lg={12}>
+                                        <TextField
+                                            id="outlined-name"
+                                            label="Message"
+                                            fullWidth
+                                            multiline
+                                            value={message}
+                                            onChange={e => setState({ message: e.target.value})}
+                                            margin="normal"
+                                            variant="outlined"
+                                        />
+                                    </Col>
+                                </Row>
+                                <Button
+                                    className="button button-accent"
+                                    onClick={handleFormSubmit}
+                                >   Submit
+                                    </Button>
+                            </CardContent>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+            <br />
+        </React.Fragment>
+    )
 }
+
+    // state = {
+    //     firstName: " ",
+    //     lastName: " ",
+    //     email: " ",
+    //     message: " "
+    // }
+
+    // handleChange = name => event => {
+    //     this.setState({
+    //         [name]: event.target.value,
+    //     });
+    // };
+
+    // handleFormSubmit = e => {
+    //     e.preventDefault();
+    //     console.log(this.state);
+    // };
+
+    
 
 const styles = {
     card: {
