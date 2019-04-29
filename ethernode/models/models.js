57d148b3a7767eb191460667500474843c61341d
Blora616 <Blora616@gmail.com>
2019-04-28 23:28:45 -0400

    models

--- ethernode/models/models.js
+++ ethernode/models/models.js
@@ -2,52 +2,52 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const accountsSchema = mongoose.Schema({
-    userEmail:  { type: String, required: true, unique: true },
-    password:     { type: String },
-    userRole:     { type: String, required: true },
-    userFirst:    { type: String },
-    userLast:     { type: String },
-    nodes:        { type: Array, "default":[] },
-    orders:         { type: Object,
-        orderId:     { type: String },
-        payType:     { type: String },
-        txResultCode: { type: String },
-        txCode: { type: String },
-        txText: { type: String },
-        items:     { type: Object, 
-            itemID:     { type: String },
-            itemName: { type: String },
-            itemDesc: { type: String },     
-            itemQty:     { type: String },     
-            itemCost: { type: String }
-        },
-        orderTotal:  { type: String },
-        orderStatus: { type: String }
+  userEmail: { type: String, required: true, unique: true },
+  userRole: { type: String, required: true },
+  userFirst: { type: String },
+  userLast: { type: String },
+  orders: {
+    type: Object,
+    orderId: { type: String },
+    payType: { type: String },
+    items: {
+      type: Object,
+      itemID: { type: String },
+      itemName: { type: String },
+      itemDesc: { type: String },
+      itemQty: { type: String },
+      itemCost: { type: String }
     },
-    address: { type: Object,
-        billing: { type: Object,
-            company:     { type: String },
-            address1: { type: String },
-            address2: { type: String },
-            city:            { type: String },
-            state:        { type: String },
-            postal:        { type: String },
-            country:  { type: String },
-            phone:        { type: String }
-        },
-        shipping: { type: Object,
-            company:     { type: String },
-            address1: { type: String },
-            address2: { type: String },
-            city:            { type: String },
-            state:        { type: String },
-            postal:        { type: String },
-            country:  { type: String },
-            phone:        { type: String }
-        }
+    orderTotal: { type: String },
+    orderStatus: { type: String }
+  },
+  address: {
+    type: Object,
+    billing: {
+      type: Object,
+      company: { type: String },
+      address1: { type: String },
+      address2: { type: String },
+      city: { type: String },
+      state: { type: String },
+      postal: { type: String },
+      country: { type: String },
+      phone: { type: String }
+    },
+    shipping: {
+      type: Object,
+      company: { type: String },
+      address1: { type: String },
+      address2: { type: String },
+      city: { type: String },
+      state: { type: String },
+      postal: { type: String },
+      country: { type: String },
+      phone: { type: String }
     }
+  }
 });
 
 const User = mongoose.model("User", userSchema);
 
-module.exports = Book;
+module.exports = User;
